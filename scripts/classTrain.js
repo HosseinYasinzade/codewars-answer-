69f9f21c08aedc618a0bc8d84cdb75778ba5ecba
Hossein <hosseinyasinzadeh2002@gmail.com>
2024-10-01 13:25:36 +0330

    coding train2

--- scripts/classTrain.js
+++ scripts/classTrain.js
@@ -1,4 +1,18 @@
-const bonusTime = (salary, bonus) =>
-  bonus === true ? `£${salary * 10}` : `£${salary}`;
+// https://www.codewars.com/kata/56f6ad906b88de513f000d96
 
-console.log(bonusTime(10000, true));
+// const bonusTime = (salary, bonus) =>
+//   bonus === true ? `£${salary * 10}` : `£${salary}`;
+
+// console.log(bonusTime(10000, true));
+
+// ==================================================================
+
+// https://www.codewars.com/kata/57eadb7ecd143f4c9c0000a3
+
+const abbrevName = (name) =>
+  name
+    .split(" ")
+    .map((item) => item[0].toUpperCase())
+    .join(".");
+
+console.log(abbrevName("Hossein Yasinzadeh"));
